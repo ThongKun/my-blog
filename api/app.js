fee07e9d70042c5dfe6069d0be792f24ada54188
Văn Thông Lê <thonglevan131@gmail.com>
2020-05-16 18:08:02 +0700

    make static folder publicly available

--- api/app.js
+++ api/app.js
@@ -3,13 +3,11 @@ const app = express();
 const Post = require('./models/posts');
 const postsData = new Post();
 
-const posts = [{
-    "id": "1581461442206",
-    "title": "This is a New Blog Post",
-    "content": "This is the content! ",
-    "post_image": "uploads/post-image-1581461442199.jpg",
-    "added_date": "1581461442206"
-}]
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    next()
+})
+app.use('/uploads', express.static('uploads'))
 
 app.get("/api/posts", (req, res) => {
     res.status(200).send(postsData.get())
