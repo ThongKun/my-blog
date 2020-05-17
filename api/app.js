dadf608be9780f5d1e91075b6d5fcb2754abae19
Văn Thông Lê <thonglevan131@gmail.com>
2020-05-17 19:10:52 +0700

    add api create post

--- api/app.js
+++ api/app.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const app = express();
 const Post = require('./models/posts');
+const multer = require('multer')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+})
+const getExt = (mimeType) => {
+    switch (mimeType) {
+        case "image/png":
+            return ".png"
+        case "image/jpeg":
+            return ".jpeg"
+    }
+}
+var upload = multer({ storage : storage })
+
 const postsData = new Post();
+
+app.use(express.json())
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
@@ -21,6 +43,18 @@ app.get('/api/posts/:post_id', (req, res) => {
     } else {
         res.status(404).send("Not Found")
     }
+})
+
+app.post('/api/posts', upload.single("post-image"), (req, res) => {
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": req.file.path.replace("\\", "/"),
+        "added_date": `${Date.now()}`
+    }
+    postsData.add(newPost)
+    res.status(201).send(newPost)
 })
 
 app.listen(3000, () => console.log("listening on http://localhost:3000"));
--- frontend/js/post.js
+++ frontend/js/post.js
@@ -9,13 +9,35 @@ const API_BASE_URL = "http://localhost:3000/";
 
 window.onload = () => {
     getPost();
+    getPostIdParam()
+}
+
+getPostIdParam = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get("id")
 }
 
 const getPost = () => {
     // CODE GOES HERE
+    const postId = getPostIdParam();
+    const url = `${API_URL}${postId}`;
+    fetch(url, {
+        method: 'GET'
+    }).then(response => {
+        return response.json()
+    }).then(data => {
+        buildPost(data)
+    })
 }
 
 const buildPost = (data) => {
     // HINT: Convert the date number to a Date string 
+    const postImage = `${API_BASE_URL}${data.post_image}`;
+    document.querySelector("header").style.backgroundImage = `url(${postImage})`
+    const postDate = new Date(parseInt(data.added_date)).toDateString();
+    document.getElementById("individual-post-title").innerHTML = data.title;
+    document.getElementById("individual-post-date").innerHTML = `Published on ${postDate}`;
+    document.getElementById("individual-post-content").innerHTML = data.content;
 }
 
